7551dd6980282fb60e1cd324da700ada497ffaf0
jacoobes <jngu195@lsu.edu>
2024-12-02 14:20:37 -0600

    Asdf

--- src/y2024/d2.clj
+++ src/y2024/d2.clj
@@ -1,15 +1,18 @@
 (ns y2024.d2
   (:require [clojure.test :as t :refer [deftest]]
-            [clojure.string :as string]))
+            [clojure.string :as string]
+            [clojure.math :as math]
+            ))
 
 (defn diffs [xs]
-  (map #(- % %2) xs (rest xs)))
+  (map - xs (rest xs)))
 
 (defn strictly-inc? [xs] 
   (every? pos? xs) )
 
 (defn strictly-dec? [xs] 
   (every? neg? xs) )
+
 ;; PROBLEM LINK https://adventofcode.com/2024/day/2
 (defn generator
   "The generator fn is used to parse your input into. The output of this fn will be passed into each of the solving fns"
@@ -18,7 +21,7 @@
        (string/split-lines input)))
 
 (defn issafe? [arr]
-  (and (every? #(and (>= % -3) (<= % 3) ) arr)
+  (and (every? #(<= 1 (abs %) 3) arr)
        (or (strictly-inc? arr) (strictly-dec? arr))))
 
 (defn solve-part-1
@@ -26,7 +29,7 @@
   [input]
   (->> input
        (map diffs)
-       (filter issafe? )
+       (filter issafe?)
        (count)))
 
 (defn strictly-inc? [xs] 
@@ -35,20 +38,29 @@
 (defn strictly-dec? [xs] 
   (every? neg? xs))
 
+(defn perms [c] 
+  (map #(concat (take % c) (drop (inc %)  c))
+     (range (count c))))
+
 (defn solve-part-2
   "The solution to part 2. Will be called with the result of the generator"
   [input]
-  (let [[safe, maybesafe ]  (->> input (map diffs)
-                                       ((juxt filter remove) issafe?))
-         maybemaybesafe (->> maybesafe 
-                             (filter #(every? (fn [e] (and (>= e -3) (<= e 3) )) % ))) ]
-        
-      (+ (count safe ) (count (map (fn [xs]  (or (= 1 (count (filter pos? xs)))
-                                                 (= 1 (count (filter zero? xs)))
-                                                 (= 1 (count (filter neg? xs))))) maybemaybesafe)))
-     ))
+  (let [safe-reports 
+        (count 
+         (filter 
+          (fn [report]
+            (or 
+             ;; Original report is safe
+             (issafe? (diffs report))
+             ;; Check if removing any single level makes it safe
+             (some 
+              #(issafe? (diffs %)) 
+              (map #(concat (take % report) (drop (inc %) report)) 
+                   (range (count report))))))
+          input))]
+    safe-reports))
 
- 
+
 ;; Tests
 ;; Use tests to verify your solution. Consider using the sample data provided in the question
 
@@ -64,6 +76,6 @@
 (deftest sample-test
   (t/is (= 2 (solve-part-1 (generator sample-data)))))
 
-
+; not 399, 503, 386, 313 , 430
 (deftest sample-test-2
   (t/is (= 4 (solve-part-2 (generator sample-data)))))
