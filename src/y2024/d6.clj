c1e60a8835ae3a02cee1c5f8fb0ec182e920e7b0
jacoobes <jngu195@lsu.edu>
2024-12-06 21:57:36 -0600

    ffday6pt2

--- src/y2024/d6.clj
+++ src/y2024/d6.clj
@@ -41,31 +41,60 @@
     :east  :south
     :south :west
     :west :north))
+(defn is-rect? [points]
+  
+  )
+(defn walk-out-maze [input]
+  (let [[grid-map position] input]
+    (loop [orientation :north 
+           cur-position position
+           walked #{cur-position} ] 
+      (let [new-pos (vec (map + (offset orientation) cur-position))
+            ch (get-in grid-map (vec new-pos)) 
+            #_ (println walked new-pos) ]
+        (if ch 
+          (cond (= ch \#) (recur (rotate orientation) cur-position   walked) 
+                 :else (recur orientation new-pos  (conj walked new-pos)))
+          walked)))))
 
 (defn solve-part-1
   "The solution to part 1. Will be called with the result of the generator"
   [input]
-  (let [[grid-map position]  input ]
-  (loop [orientation :north 
-         cur-position position
-         walked #{cur-position}] 
-    (let [new-pos (vec (map + (offset orientation) cur-position))
-          ch (get-in grid-map (vec new-pos))]
-      (if ch
-        (if (= ch \#)
-          (recur (rotate orientation) cur-position walked) 
-          (recur orientation new-pos (conj walked new-pos)))
-        (count walked) )))))
+  (let [[grid-map position]  input
+        positions-walked (walk-out-maze input)]
+    (count positions-walked )))
+
 
 (defn solve-part-2
   "The solution to part 2. Will be called with the result of the generator"
   [input]
-  (let [[grid-map position]  input ]
-    grid-map
-  ))
+  (let [[grid-map position]  input
+        walked (solve-part-1 input)
+        all-grids (for [[i r]  (map-indexed vector grid-map)
+                        [j c]  (map-indexed vector r) 
+                        :when (not= c \#)]
+                        [(assoc-in grid-map [i j] \#) position] )
+        ]
+    (->> (map walk-out-maze all-grids)
+         (filter nil?)
+         count)  
+    ))
+
+(= [6 4] [6 4])
+(let [[grid-map position]  (generator sample-data-1) 
+       walked (walk-out-maze [grid-map position])
+       all-grids (for [[i r]  (map-indexed vector grid-map)
+                       [j c]  (map-indexed vector r) 
+                       :when  (and (not= c \#) (not= position [i j]) (some? (walked [i j]))) ]
+                       [(assoc-in grid-map [i j] \o) position] ) 
+      _ (clojure.pprint/pprint (nth all-grids 15)) ] 
+   (get all-grids 4)
+  ) 
 
 ;; Tests
 ;; Use tests to verify your solution. Consider using the sample data provided in the question
 
 (deftest sample-test
   (t/is (= 41 (solve-part-1 (generator sample-data-1)) )))
+(deftest sample-test-2
+  (t/is (= 6 (solve-part-2 (generator sample-data-1)) )))
