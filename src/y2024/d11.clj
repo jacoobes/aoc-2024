4bee5b4e822d8e282dd06edc1f9d88976d35edc1
jacoobes <jngu195@lsu.edu>
2024-12-11 18:56:40 -0600

    s

--- src/y2024/d11.clj
+++ src/y2024/d11.clj
@@ -15,34 +15,46 @@
   [input]
   (vec (re-seq #"\d+" input)) )
 
+;https://stackoverflow.com/a/7089841
+(def trim-leading-zeros 
+ (memoize  (fn [^String source]
+    (loop [i 0 
+           s source] 
+      (if (= i (count s))
+       "0"
+       (if (not= \0 (.charAt s i))
+        (subs s i)
+        (recur (inc i)  s)) )))))
 
-(def parse-halves 
-  (memoize (comp str parse-long))) ;ensure that each
+#_(trim-leading-zeros a)
 
 (def split-number 
-   (memoize (fn [num]
-    (let [digits num ; Convert the number to a string
-          half  (quot (count digits) 2)] ; Calculate the midpoint
-      [(subs digits 0 half) (subs digits half)])))) ; Split into halves
+   (memoize (fn [digits]
+    (let [half (quot (count digits) 2)] ; Calculate the midpoint
+      [ (subs digits 0 half)
+        (trim-leading-zeros (subs digits half))   ])))) ; Split into halves
 
 (def year-parser (memoize (fn  [cur]
                            (str (* 2024 (parse-long cur))))))
 
+
 (def blicky (fn [acc cur]
-              (cond 
-                (= "0" cur) (conj acc "1")
-                (even? (count cur))  (let [v (split-number cur) #_ (println cnt ) 
-                                           #_ (println (map parse-halves v)) ]
-                                           (into acc (map parse-halves) v))
-                :else (conj acc (year-parser cur)))))
+      (cond 
+        (= "0" cur) (conj! acc "1")
+        (= "1" cur) (conj! acc "2024")
+        (even? (count cur))  (let [v (split-number cur)
+                                   #_ (println (get v 0) (trim-leading-zeros (get v 1))) ]
+                               (conj! (conj! acc (get v 0)) (get v 1)))
+        :else (conj! acc (year-parser cur)))))
 
 (defn light-years [n input] 
   (loop [cnt 0
-         result input] 
+         result input ] 
+      (println cnt)
       (if (= cnt n)
         result
-         (recur (inc cnt) 
-              (reduce blicky [] result) )  )))
+        (recur (inc cnt) 
+              (persistent! (reduce blicky (transient []) result))  )  )))
 
 (defn solve-part-1
   "The solution to part 1. Will be called with the result of the generator"
@@ -53,10 +65,19 @@
   "The solution to part 2. Will be called with the result of the generator"
   [input]
   (count (light-years 75 input)))
+
 ;; Tests
 ;; Use tests to verify your solution. Consider using the sample data provided in the question
 #_(println (count (solve-part-1 (generator sample-data))))
+
+#_(let [data (generator (slurp "input/2024/11.txt"))]
+      (count (solve-part-2 data)))
+
 (deftest sample-test
   (t/is (= 22 (solve-part-1 (generator sample-data)) )))
+
+#_(defn -main [& args] 
+  (println (count (light-years 75 (generator (slurp "input/2024/11.txt")))))  )
+
 
 
